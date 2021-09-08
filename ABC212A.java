49dbc7d6e6ac918816f37ad91861d0545d9ea54b
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-09-08 14:45:22 +0900

    Add files via upload

---
+++ ABC212A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = Integer.parseInt(sc.next());
+    int b = Integer.parseInt(sc.next());
+    if (a>0 && b==0) {
+      System.out.println("Gold");
+    }else if (a==0 && b>0) {
+      System.out.println("Silver");
+    }else{
+      System.out.println("Alloy");
+    }
+    }
+  }
